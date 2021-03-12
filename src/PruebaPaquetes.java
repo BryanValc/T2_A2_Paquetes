39aecadf36981000091963d6ef503a6984a9a054
Bryan Valdez <bryan501th@gmail.com>
2021-03-11 18:29:10 -0600

    Se empezó a crear la interfaz Paquetes

--- src/PruebaPaquetes.java
+++ src/PruebaPaquetes.java
@@ -1,12 +1,80 @@
+import java.awt.Component;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.SwingUtilities;
+import javax.swing.WindowConstants;
+
 import edu.tecjerez.topicos.figuras.dosDimensiones.*;
 import edu.tecjerez.topicos.figuras.tresDimensiones.*;
 
+class Paquetes extends JFrame implements ActionListener{
+	
+	GridBagLayout gbl = new GridBagLayout();
+	GridBagConstraints gbc = new GridBagConstraints();
+
+	JComboBox<String> comboTipo = new JComboBox<String>();
+	JButton area,perimetro,volumen;
+	
+	public Paquetes() {
+		getContentPane().setLayout(gbl);
+		setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		setLocationRelativeTo(null);
+		setTitle("GUI-GridBagLayout");
+		gbc.fill=GridBagConstraints.BOTH;
+		setVisible(true);
+		
+		String tipos[] = {"Rombo","Circulo","Elipse","Piramide","Cono"};
+		comboTipo = new JComboBox(tipos);
+		inst(comboTipo, 0, 0, 3, 3, GridBagConstraints.NONE);
+		
+		inst(new JLabel("Calcular"), 3, 0, 1, 3, GridBagConstraints.NONE);
+		
+		area = new JButton("Area");
+		area.addActionListener(this);
+		inst(area, 6, 0, 2, 1, GridBagConstraints.NONE);
+		
+		perimetro = new JButton("Perimetro");
+		perimetro.addActionListener(this);
+		inst(perimetro, 6, 1, 2, 1, GridBagConstraints.NONE);
+		
+		volumen = new JButton("Volumen");
+		volumen.addActionListener(this);
+		inst(volumen, 6, 2, 2, 1, GridBagConstraints.NONE);
+		
+		
+		pack();
+	}
+	
+	public void inst(Component cmp,int gridx, int gridy, int gridwidth, int gridheight, int fill) {
+		gbc.gridx=gridx;
+		gbc.gridy=gridy;
+		gbc.gridwidth=gridwidth;
+		gbc.gridheight=gridheight;
+		gbc.fill=fill;
+		gbl.setConstraints(cmp, gbc);
+		add(cmp);
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
 
 public class PruebaPaquetes {
 	
 	public static void main(String[] args) {
 		
-		Rombo rmb = new Rombo(8, 6);
+		/*Rombo rmb = new Rombo(8, 6);
 		System.out.println(rmb.obtenerArea());
 		System.out.println(rmb.obtenerPerimetro());
 		
@@ -24,7 +92,14 @@ public class PruebaPaquetes {
 		
 		Cono cn = new Cono(3,5);
 		System.out.println(cn.obtenerVolumen());
-		System.out.println(cn.obtenerArea());
+		System.out.println(cn.obtenerArea());*/
+		
+		SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				new Paquetes();
+			}
+		});
+		
 		
 	}
 
